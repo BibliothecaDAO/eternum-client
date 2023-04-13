b8d757aa165a78328bb9eac84c177f68a5142cbc
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-04-13 20:29:39 +1000

    add tail

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <h1 className="text-3xl font-bold underline">React TypeScript Template</h1>
     </div>
   )
 }
