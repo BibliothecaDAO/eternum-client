cfb060711fca415f5364e8b932959c69b004be7f
0x1337 <deadw00d@mail.ru>
2023-05-09 19:20:56 +0700

    feat: flower pattern

--- src/containers/BaseContainer.tsx
+++ src/containers/BaseContainer.tsx
@@ -10,6 +10,7 @@ export const BaseContainer = ({ children, className }: BaseContainerProps) => {
     return (
         <div className={clsx("p-2 flex shadow-black/30 shadow-md flex-col border-2 border-white border-solid border-opacity-20 rounded-xl  container-bg-gradient backdrop-blur-sm", className)}>
             {children}
+            <div className="absolute top-0 left-0 z-0 w-full h-full pointer-events-none rounded-xl flower-pattern"></div>
         </div>
     );
 };
