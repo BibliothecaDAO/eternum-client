09e16302fb3f840042cc7e41bda6d2950701f485
0x1337 <deadw00d@mail.ru>
2023-05-09 21:40:30 +0700

    feat: tabs layout

--- src/components/RealmManagementComponent.tsx
+++ src/components/RealmManagementComponent.tsx
@@ -1,12 +1,14 @@
 import { BaseContainer } from "../containers/BaseContainer"
 import { SecondaryContainer } from "../containers/SecondaryContainer"
+import Tabs from "./Tabs"
 
 const RealmManagementComponent = () => {
     return (
         <div className="flex w-full mt-0.5 relative">
-            <BaseContainer className="w-full min-h-[400px]" />
+            <BaseContainer className="w-full min-h-[400px]">
+                <Tabs />
+            </BaseContainer>
             <SecondaryContainer className="absolute bottom-0 min-w-[300px] min-h-[200px] translate-x-full -right-2">
-
             </SecondaryContainer>
         </div>
     )
