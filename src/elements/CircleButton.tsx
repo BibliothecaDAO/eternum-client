7608d4e2c243b6215913f1f4c8160948054e01d5
0x1337 <deadw00d@mail.ru>
2023-05-09 20:25:36 +0700

    fix: circle button border

--- src/elements/CircleButton.tsx
+++ src/elements/CircleButton.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 const CircleButton = ({ children, className }: { children?: React.ReactNode, className?: string }) => {
     return (
-        <button className={clsx("flex hover:scale-105 transition-transform duration-100 cursor-pointer items-center justify-center w-12 h-12 bg-black/60 text-white/70 rounded-full shadow-md shadow-black/50", className)}>
+        <button className={clsx("flex hover:scale-105 transition-transform duration-100 cursor-pointer items-center justify-center w-12 h-12 bg-black/60 text-white/70 rounded-full shadow-md border-2 border-white/10 shadow-black/50", className)}>
             {children}
         </button>
     );
